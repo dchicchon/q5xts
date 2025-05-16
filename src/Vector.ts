97db8069bb55065bba8c89ae9082b207768431a2
Daniel Chicchon <danielchicchon@gmail.com>
2025-05-16 10:53:07 -0700

    Update method arg to optional in Vector.set

--- src/Vector.ts
+++ src/Vector.ts
@@ -250,7 +250,7 @@ export class Vector {
     this.cacheNorm = null;
   }
 
-  set(_x: number, _y: number, _z: number) {
+  set(_x: number, _y: number, _z?: number) {
     this.x = _x || 0;
     this.y = _y || 0;
     this.z = _z || 0;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Color } from './Color';
+import { Vector } from './Vector';
+import { Q5 } from './Q5';
+
+export {
+    Color,
+    Vector,
+    Q5
+}
